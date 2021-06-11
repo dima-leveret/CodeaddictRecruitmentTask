0d6a9bee84c1b0f3b65e4b427d43ad6e8d93102e
Dima <dimazayac@gmail.com>
2021-06-11 09:46:43 +0200

    created burger menu

--- crt-app/src/components/NavBar.js
+++ crt-app/src/components/NavBar.js
@@ -1,18 +1,60 @@
 import React from "react";
+
 import '../style/NavBar.css';
-import TeamifyLogo from '../img/teamify.svg'
+
+import TeamifyLogo from '../img/teamify.svg';
+import openBurger from '../img/openBurger.svg';
+import closeBurger from '../img/closeBurger.svg';
 
 class NavBar extends React.Component {
 
+    state = {
+        burger: openBurger,
+        bugregMenuClass: 'closed-burger-nav',
+
+    }
+
+    handleOnBurgerClick = () => {
+        if (this.state.burger === openBurger) {
+            this.setState({
+                burger: closeBurger,
+                bugregMenuClass: 'opened-burger-nav',
+            })
+        }
+
+        if (this.state.burger === closeBurger) {
+            this.setState({
+                burger: openBurger,
+                bugregMenuClass: 'closed-burger-nav',
+            })
+        }
+        
+    }
     
     render () {
         return (
-            <div className="Landing-Page">
-                <div className="Background">
-                    <header>
-                        <div className="header-container">
-                            <img className="Teamify" src={TeamifyLogo} alt="Teamify-logo"/>
-                            <nav className="nav">
+                <header>
+                    <div className="header-container">
+                        <img className="teamify-img" src={TeamifyLogo} alt="Teamify-logo"/>
+                        <nav className="nav">
+                            <ul>
+                                <li>Freelancers</li>
+                                <li>About Us</li>
+                                <li>Case-Study</li>
+                                <li>Reviews</li>
+                                <li>Newsletter</li>
+                            </ul>
+                        </nav>
+
+                        <div  className='burger-menu'>
+                            <img 
+                            src={this.state.burger} 
+                            alt='burger-menu' 
+                            className='burger'
+                            onClick={() => this.handleOnBurgerClick()} 
+                            />
+
+                            <nav className={this.state.bugregMenuClass}>
                                 <ul>
                                     <li>Freelancers</li>
                                     <li>About Us</li>
@@ -22,10 +64,9 @@ class NavBar extends React.Component {
                                 </ul>
                             </nav>
                         </div>
-                        <hr/>
-                    </header>
-                </div>
-            </div>
+                    </div>
+                <hr/>
+            </header>
         )
     }  
 }
