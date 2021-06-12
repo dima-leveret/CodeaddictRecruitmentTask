387a13c418c72c7f60d5659068978026a9364993
Dima <dimazayac@gmail.com>
2021-06-12 13:33:22 +0200

    finished with subscribe component, created and finished with footer component, finished with subscribe-footer component

--- crt-app/src/components/subscribe-footer/Footer.js
+++ crt-app/src/components/subscribe-footer/Footer.js
@@ -1,10 +1,25 @@
-import React from "react"
+import React from "react";
+
+import "../../style/subscribe-footer/Footer.css";
+
+import twitterLogo from "../../img/twitter.svg"
 
 function Footer() {
     return (
-        <div>
-            Footer
-        </div>
+        <footer>
+            <div className='footer-container'>
+                <p className='teamify-cr'>© Teamify</p>
+                <p className='copyrights'>Copyrights @ Teamify, All rights reserved</p>
+                <a href='https://twitter.com/' target='_blank' rel="noreferrer" >
+                    <img
+                    className='twitter-logo' 
+                    src={twitterLogo} 
+                    alt='twitter-logo' 
+                    />
+                </a>
+            </div>
+        </footer>
+        
     )
 }
 
--- crt-app/src/components/subscribe-footer/Subscribe.js
+++ crt-app/src/components/subscribe-footer/Subscribe.js
@@ -2,13 +2,48 @@ import React from "react";
 
 import "../../style/subscribe-footer/Subscribe.css"
 
-function Subscribe() {
-    return (
-        <div className='subscribe-container'>
-           <h2>Subscribe to get instant alerts</h2>
-           <p>Don’t wanna miss something? Subscribe right now and get special deals and monthly newsletter</p>
-        </div>
-    )
+class Subscribe extends React.Component {
+
+    state = {
+        inputValue: '',
+    }
+
+    handleOnInputChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
+    handleOnSubmitClick = () => {
+        console.log(this.state.inputValue);
+        this.setState ({
+            inputValue: ''
+        })
+    }
+
+    render() {
+        return (
+            <div className='subscribe-container'>
+               <h2>Subscribe to get instant alerts</h2>
+               <p>Don’t wanna miss something? Subscribe right now and get special deals and monthly newsletter</p>
+               <form className='subscribe-input-container'>
+                    <input
+                    value={this.state.inputValue}
+                    type='email'
+                    placeholder='Enter your email address'
+                    id='email'
+                    name='inputValue'
+                    onChange={this.handleOnInputChange}
+                    />
+                    <label
+                    onClick={() => this.handleOnSubmitClick()} 
+                    htmlFor='email' 
+                    >Submit</label>
+               </form>
+            </div>
+        )
+    }
+    
 }
 
 export default Subscribe;
