b8c83c2dcd6b87ada67995d1aa4e0c09d5a5ab25
Dima <dimazayac@gmail.com>
2021-06-16 15:30:05 +0200

    created Pagination component, almost dane with pagination

--- crt-app/src/components/ListOfUsers/UsersList.js
+++ crt-app/src/components/ListOfUsers/UsersList.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import UserCard from './UserCard';
+import Pagination from './Pagination'
 
 import '../../style/ListOfUsers/UsersList.css'
 
@@ -12,16 +13,32 @@ import { fetchUsers } from '../../state/users';
 
 class UsersList extends React.Component {
 
+    state = {
+        currentPage: 1,
+        usersPerPage: 12,
+    }
+
     componentDidMount(){
         this.props.fetchUsers();
     }
 
     render() {
+
+        const lastUserIndex = this.state.currentPage * this.state.usersPerPage;
+        const firstUserIndex = lastUserIndex - this.state.usersPerPage;
+        const currentUsers = this.props.users.slice(firstUserIndex, lastUserIndex);
+
+        const paginate = pageNamber => {
+            this.setState({
+                currentPage: pageNamber,
+            })
+        }
+
         return(
             <div className='users' >
                 <div className='users-list' >
                     {
-                        this.props.users
+                        currentUsers
                         .map(user => (
                             <UserCard
                             key={user.id}
@@ -35,14 +52,19 @@ class UsersList extends React.Component {
                 <div className='showing' >
                     <p className='showing-number' >Showing: {this.props.users.length} </p>
                     <div className='showing-buttons' >
-                        <button>
+                        <button className='btn btn-left' >
                             <img src={arrowLeft} alt='arrowLeft' />
                         </button>
-                        <button>  
+                        <button className='btn btn-right' >  
                             <img src={arrowRight} alt='arrowRight' />  
                         </button>
                     </div>
                 </div>
+                <Pagination
+                    usersPerPage={this.state.usersPerPage}
+                    totalUsers={this.props.users.length}
+                    paginate={paginate}
+                />
             </div>
         )
     }
